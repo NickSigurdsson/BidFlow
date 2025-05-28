92f64899e5bdbbc7eff2418a850fc71ae24e8c8f
Nicholas Sigurdsson <nicholassigurdsson@gmail.com>
2025-05-27 20:41:56 -0400

    fixed the hero and testimonial sections:

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,9 +9,9 @@ export function Footer() {
     <footer className="bg-slate-50">
       <Container>
         <div className="py-16">
-          <Logo className="mx-auto h-10 w-auto" />
-          <nav className="mt-10 text-sm" aria-label="quick links">
+            <nav className="mt-10 text-sm" aria-label="quick links">
             <div className="-my-1 flex justify-center gap-x-6">
+              <NavLink href="#Home">BidFlow</NavLink>
               <NavLink href="#features">Features</NavLink>
               <NavLink href="#testimonials">Testimonials</NavLink>
               <NavLink href="#pricing">Pricing</NavLink>
